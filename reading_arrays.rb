afe58a968a46a8199841d31c095c8dd2da10b9c7
Natalie Emanuel <natalieazana@gmail.com>
2018-07-11 15:17:18 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,37 +1,37 @@
-STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"].upcase
+STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
 
 def first_student_by_index
-  puts "#{STUDENT_NAMES[0]}"
+  return "#{STUDENT_NAMES[0]}"
 end
 
 first_student_by_index
 
 def fourth_student_by_index
-  puts "#{STUDENT_NAMES[3]}"
+  return "#{STUDENT_NAMES[3]}"
 end
 
 fourth_student_by_index
 
 def last_student_by_index
-  puts "#{STUDENT_NAMES[5]}"
+  return "#{STUDENT_NAMES[5]}"
 end
 
 last_student_by_index
 
 def first_student_by_method
-  puts "#{STUDENT_NAMES.first}"
+  return "#{STUDENT_NAMES.first}"
 end
 
 first_student_by_method
 
 def last_student_by_method
-  puts "#{STUDENT_NAMES.last}"
+  return "#{STUDENT_NAMES.last}"
 end
 
 last_student_by_method
 
 def first_second_and_third_students
-  puts "#{STUDENT_NAMES[0,1,2]}"
+  return "#{STUDENT_NAMES[0,1,2]}"
 end
 
 first_second_and_third_students
