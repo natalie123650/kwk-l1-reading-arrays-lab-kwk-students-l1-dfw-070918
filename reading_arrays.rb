fa632597b6de3df0e7ac7e630eafe1a9cc842960
Natalie Emanuel <natalieazana@gmail.com>
2018-07-11 15:05:55 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -11,22 +11,34 @@ def first_student_by_index
   puts "#{STUDENT_NAMES[0]}"
 end
 
+fourth_student_by_index
+
 def fourth_student_by_index
   puts "#{STUDENT_NAMES[3]}"
 end
+
+fourth_student_by_index
 
 def last_student_by_index
   puts "#{STUDENT_NAMES[5]}"
 end
 
+last_student_by_index
+
 def first_student_by_method
   puts "#{STUDENT_NAMES.first}"
 end
+
+first_student_by_method
 
 def last_student_by_method
   puts "#{STUDENT_NAMES.last}"
 end
 
+last_student_by_method
+
 def first_second_and_third_students
   "#{STUDENT_NAMES[0,1,2]}"
 end
+
+first_second_and_third_students
