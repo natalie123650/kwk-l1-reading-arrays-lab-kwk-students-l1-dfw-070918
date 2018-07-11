4b566a4ed93680ad0d5fa91745e3c741bada3692
Natalie Emanuel <natalieazana@gmail.com>
2018-07-11 15:18:28 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,13 +1,13 @@
 STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
 
 def first_student_by_index
-  return "#{STUDENT_NAMES[0]}"
+  return STUDENT_NAMES[0]
 end
 
 first_student_by_index
 
 def fourth_student_by_index
-  return "#{STUDENT_NAMES[3]}"
+  return STUDENT_NAMES[3]
 end
 
 fourth_student_by_index
@@ -19,19 +19,19 @@ end
 last_student_by_index
 
 def first_student_by_method
-  return "#{STUDENT_NAMES.first}"
+  return STUDENT_NAMES.first
 end
 
 first_student_by_method
 
 def last_student_by_method
-  return "#{STUDENT_NAMES.last}"
+  return STUDENT_NAMES.last
 end
 
 last_student_by_method
 
 def first_second_and_third_students
-  return "#{STUDENT_NAMES[0,1,2]}"
+  return STUDENT_NAMES[0,1,2]
 end
 
 first_second_and_third_students
